eb3be94f25b6ca8f5b088d72a891e21229aa0175
sysu19351013 <61168904+sysu19351013@users.noreply.github.com>
2020-02-18 07:57:47 +0800

    更改网址logo
    第一次使用gthub上传文件

---
+++ 程序2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+
+
+	//5.charÓëintµÄ»¥»»ÔËËã
+	int tmp;
+	tmp=65;
+	printf("tmp = %d\n",tmp);
+	printf("tmp = %c\n",tmp);
+	printf("tmp+5 = %c\n",tmp+5);
+	getchar();
+
+}
